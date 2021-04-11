2739634e69e389605fd7d21ad239edf39788f2ea
Taianam <taiana.monteirof@gmail.com>
2021-04-10 21:23:32 -0300

    Exerciício

--- Fase- 3/10-maisQueMil.js
+++ Fase- 3/10-maisQueMil.js
@@ -1,1 +1,8 @@
-// var multiploDeQuatro =
+
+//Resultado da soma dos 4 primeiros múltiplos de 4 acima de mil, subtraindo dos 4 primeiros números primos a partir do zero.
+
+let mil = 1004 + 1008 + 1012 + 1016;
+let numero = 2 + 3 + 5 + 7; 
+let resultado = mil - numero;
+
+console.log("A soma dos 4 primeiros múltiplos de 4 acima de mil, menos os 4 primeiros números primos é: " + resultado + " ! :)");
